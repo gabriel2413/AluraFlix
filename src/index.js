158ad678eae776a1eccc6e0d5ec18669facf8e4c
ggabriel2413 <ggabriel2413@gmail.com>
2020-07-31 06:33:51 -0300

    path change

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import CadastroVideo from './pages/Cadastro/Video';
-import CadastroCategoria from './pages/Cadastro/Categoria';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
 import Pagina404 from './pages/Page404';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact />
-      <Route path="/Cadastro/video" component={CadastroVideo} />
-      <Route path="/Cadastro/categoria" component={CadastroCategoria} />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component={Pagina404} />
     </Switch>
   </BrowserRouter>,
